63ba74d383d40b2406f72da684cd9186195ae217
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-06 19:46:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,10 +81,12 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
+      ORDER BY students.id
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
 
   def self.all_students_in_grade_X(grade)
